a3503e8251378669844436b108decfb3c2399161
MaryamMairaj123 <36111301+MaryamMairaj123@users.noreply.github.com>
2024-08-30 22:31:43 +0400

    Update app.py

--- Examples/first-docker-file/app.py
+++ Examples/first-docker-file/app.py
@@ -1,17 +1,20 @@
 def star_diamond():
-	for row in range(4):
-		for space in range(3-row):
-			print(' ', end='')
-		for star in range(row+1):
-			print('*',end='')
-		for star in range(row):
-			print('*',end='')
-		print()
-	for row in range(3):
-		for space in range(row+1):
-			print(' ', end='')
-		for star in range(3-row):
-			print('*',end='')
-		for star in range(2-row):
-			print('*',end='')
-		print()
+    for row in range(4):
+        for space in range(3 - row):
+            print(' ', end='')
+        for star in range(row + 1):
+            print('*', end='')
+        for star in range(row):
+            print('*', end='')
+        print()
+    for row in range(3):
+        for space in range(row + 1):
+            print(' ', end='')
+        for star in range(3 - row):
+            print('*', end='')
+        for star in range(2 - row):
+            print('*', end='')
+        print()
+
+if __name__ == "__main__":
+    star_diamond()
